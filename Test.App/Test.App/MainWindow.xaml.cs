8622dd206d9ff5273aeb74357f3bd505a5cb9327
David Gómez <v-davgom@microsoft.com>
2019-05-24 14:34:53 +0200

    Add text to main window

--- Test.App/Test.App/MainWindow.xaml.cs
+++ Test.App/Test.App/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestCore.Core;
 
 namespace Test.App
 {
@@ -23,6 +24,12 @@ namespace Test.App
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            myTextBlock.Text = TextService.GetText();
         }
     }
 }
